61c66aa40d4658f2ff22d520f09dbf98f3ab00b8
Marius Bock <marius.bock@gmail.com>
2024-01-09 14:55:31 +0200

    Update hosted script to be the same as local script

--- cryptopricehosted.sh
+++ cryptopricehosted.sh
@@ -2,16 +2,12 @@
 cd /home/mariusb16/cryptodailyprice
 echo "=======================================================================================" >> cryptoprice.data
 date >> cryptoprice.data
-curl -s 'https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,LTC,DASH,DOGE,BTG,SC,BCH,OMG,CVC,DCR,XVG,XMR,ADA&tsyms=USD' >> cryptoprice.data 
+curl -s 'https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,LTC,DASH,DOGE,BTG,SC,BCH,OMG,CVC,DCR,XVG,XMR,ADA,USDT,USDC,ROSE&tsyms=USD' >> cryptoprice.data 
 echo "" >> cryptoprice.data
-curl -s 'https://min-api.cryptocompare.com/data/pricemulti?fsyms=ADA,DOGE,XMR,LTC,XLM,CVC,BNB,SC&tsyms=BTC' >> cryptoprice.data 
+curl -s 'https://min-api.cryptocompare.com/data/pricemulti?fsyms=ADA,DOGE,SHIB,XMR,LTC,XLM,BNB,DASH,ETH,USDT,USDC,ROSE,LUNA,LUNC&tsyms=BTC' >> cryptoprice.data 
 echo "" >> cryptoprice.data
-curl -s 'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=ZAR&e=LUNO' >> cryptoprice.data
+curl -s 'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=ZAR&e=VALR' >> cryptoprice.data
 echo "" >> cryptoprice.data
-curl -s 'https://free.currencyconverterapi.com/api/v6/convert?q=USD_ZAR&compact=ultra&apiKey=9376cdd3b48e683ae920' >> cryptoprice.data
-echo "" >> cryptoprice.data
-curl -s 'https://free.currencyconverterapi.com/api/v6/convert?q=ZAR_THB&compact=ultra&apiKey=9376cdd3b48e683ae920' >> cryptoprice.data
-echo "" >> cryptoprice.data
-curl -s 'https://mempool.space:8999/api/v1/fees/recommended' >> cryptoprice.data
+curl -s 'https://openexchangerates.org/api/latest.json?app_id=3263b0c93523446299d17e2e6abdd748&symbols=ZAR,THB' >> cryptoprice.data
 echo "" >> cryptoprice.data
 exit 0
