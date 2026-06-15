aa8146c669cdbc8bcfd9609f365490ca760aedf6
Marius Bock <marius.bock@gmail.com>
2026-06-15 09:14:18 +0200

    Update API calls to CoinGecko and disable CryptoCompare

--- cryptoprice.sh
+++ cryptoprice.sh
@@ -13,16 +13,16 @@ fi
 
 echo "=======================================================================================" >> cryptoprice.data
 date >> cryptoprice.data
-curl -s 'https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,DOGE,TRX,ADA,NIGHT,BDAG,USDT,USDC&tsyms=USD&api_key=e67a1a47e8ce26da50c001a735d12e81b8c6b570094ead5e8b57bccd6a0aeae7' >> cryptoprice.data
+# curl -s 'https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,DOGE,TRX,ADA,NIGHT,BDAG,USDT,USDC&tsyms=USD&api_key=e67a1a47e8ce26da50c001a735d12e81b8c6b570094ead5e8b57bccd6a0aeae7' >> cryptoprice.data
+# echo "" >> cryptoprice.data
+curl -s 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,dogecoin,tron,cardano,midnight-3,blockdag,tether,usd-coin&vs_currencies=usd&x_cg_demo_api_key=CG-6ECawTrFcx92rJg7UrgtXUjb' >> cryptoprice.data
 echo "" >> cryptoprice.data
-curl -s 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,dogecoin,tron,cardano,night,blockdag,tether,usd-coin&vs_currencies=usd&x_cg_demo_api_key=CG-6ECawTrFcx92rJg7UrgtXUjb' >> cryptoprice.data
-echo "" >> cryptoprice.data
-curl -s 'https://min-api.cryptocompare.com/data/pricemulti?fsyms=ADA,NIGHT,BDAG,TRX,DOGE,BNB,ETH,USDT,USDC&tsyms=BTC&api_key=e67a1a47e8ce26da50c001a735d12e81b8c6b570094ead5e8b57bccd6a0aeae7' >> cryptoprice.data
-echo "" >> cryptoprice.data
+# curl -s 'https://min-api.cryptocompare.com/data/pricemulti?fsyms=ADA,NIGHT,BDAG,TRX,DOGE,BNB,ETH,USDT,USDC&tsyms=BTC&api_key=e67a1a47e8ce26da50c001a735d12e81b8c6b570094ead5e8b57bccd6a0aeae7' >> cryptoprice.data
+# echo "" >> cryptoprice.data
 curl -s 'https://api.coingecko.com/api/v3/simple/price?ids=cardano,midnight-3,blockdag,tron,dogecoin,binancecoin,ethereum,tether,usd-coin&vs_currencies=btc&x_cg_demo_api_key=CG-6ECawTrFcx92rJg7UrgtXUjb' >> cryptoprice.data
 echo "" >> cryptoprice.data
-curl -s 'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=ZAR&e=VALR&api_key=e67a1a47e8ce26da50c001a735d12e81b8c6b570094ead5e8b57bccd6a0aeae7' >> cryptoprice.data
-echo "" >> cryptoprice.data
+# curl -s 'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=ZAR&e=VALR&api_key=e67a1a47e8ce26da50c001a735d12e81b8c6b570094ead5e8b57bccd6a0aeae7' >> cryptoprice.data
+# echo "" >> cryptoprice.data
 curl -s 'GET' 'https://api.coingecko.com/api/v3/exchanges/valr/tickers?coin_ids=bitcoin&x_cg_demo_api_key=CG-6ECawTrFcx92rJg7UrgtXUjb' | jq '.tickers[] | select(.target == "ZAR") | {pair: (.base + "/" + .target), price: .last}' >> cryptoprice.data
 echo "" >> cryptoprice.data
 curl -s 'https://openexchangerates.org/api/latest.json?app_id=3263b0c93523446299d17e2e6abdd748&symbols=ZAR,THB,KZT' >> cryptoprice.data
